9337fc26c63a8e1374045afa159112f0227b91ce
Tejas Morajkar <tejas@mac.local>
2024-01-07 00:28:31 +0530

    solve day 24 part 2

--- day_24/never_tell_me_the_odds.py
+++ day_24/never_tell_me_the_odds.py
@@ -19,7 +19,7 @@ class Hailstone:
 
 
 class HailstoneTrajectoryAnalyzer:
-    def __init__(self, input_content, start_limit, end_limit, use_sympy=False):
+    def __init__(self, input_content, start_limit=None, end_limit=None, use_sympy=False):
         # Replace @ with , so that we get sx, sy, sz, vx, vy, vz
         # convert these number strings to int using map and create hailstone objects with these six values
         if use_sympy:
@@ -70,6 +70,20 @@ class HailstoneTrajectoryAnalyzer:
                         total_intersections += 1
         return total_intersections
 
+    def sum_rock_initial_positions(self) -> int:
+        xr, yr, zr, vxr, vyr, vzr = sympy.symbols("xr, yr, zr, vxr, vyr, vyz")
+        equations, answers = [], []
+        for idx, (sx, sy, sz, vx, vy, vz) in enumerate(self.hailstones):
+            equations.append((xr - sx) * (vy - vyr) - (yr - sy) * (vx - vxr))
+            equations.append((yr - sy) * (vz - vzr) - (zr - sz) * (vy - vyr))
+            if idx < 2:
+                continue
+            answers = [solution for solution in sympy.solve(equations) if all(val % 1 == 0 for val in solution.values())]
+            if len(answers) == 1:
+                break
+        answer = answers[0]
+        return answer[xr] + answer[yr] + answer[zr]
+
 
 def test_count_intersections():
     with open("test.txt", "r") as file:
@@ -91,9 +105,29 @@ def count_intersections_main():
         input_content = file.read()
         part_one_test = HailstoneTrajectoryAnalyzer(input_content, 200000000000000, 400000000000000)
         total_intersections = part_one_test.count_intersections()
-        print(total_intersections)
+        print(f"Total intersections {total_intersections}")
+
+
+def test_sum_rock_initial_positions():
+    with open("test.txt", "r") as file:
+        input_content = file.read()
+        part_two_test = HailstoneTrajectoryAnalyzer(input_content, use_sympy=True)
+        sum_positions = part_two_test.sum_rock_initial_positions()
+        assert sum_positions == 47, (f"Test failed for sum of rock initial positions. "
+                                     f"Expected 47 but got {sum_positions}")
+        print("Test test_sum_rock_initial_positions executed successfully!!!")
+
+
+def sum_rock_initial_positions_main():
+    with open("input.txt", "r") as file:
+        input_content = file.read()
+        part_two = HailstoneTrajectoryAnalyzer(input_content, use_sympy=True)
+        sum_position = part_two.sum_rock_initial_positions()
+        print(f"Sum of initial positions for rock {sum_position}")
 
 
 if __name__ == "__main__":
     test_count_intersections()
     count_intersections_main()
+    test_sum_rock_initial_positions()
+    sum_rock_initial_positions_main()
