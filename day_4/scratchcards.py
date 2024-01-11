e87e078f805c265218a73b83abfdb2a617d0dd24
Tejas Morajkar <tejas@mac.local>
2024-01-11 23:03:42 +0530

    delete input and test txt files

--- day_4/scratchcards.py
+++ day_4/scratchcards.py
@@ -70,12 +70,16 @@ def sum_of_points_main():
 
 
 def test_total_scratchcards():
-    with open("test.txt", "r") as input_file:
-        input_content = input_file.read()
-        part_two = ScratchcardAnalyzer(input_content)
-        actual_total = part_two.total_scratchcards()
-        assert actual_total == 30, f"Test for get total scratchcards failed. Expected 30 but got {actual_total}"
-        print("Test for get total scratchcards executed successfully!!!")
+    input_content = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+    part_two = ScratchcardAnalyzer(input_content)
+    actual_total = part_two.total_scratchcards()
+    assert actual_total == 30, f"Test for get total scratchcards failed. Expected 30 but got {actual_total}"
+    print("Test for get total scratchcards executed successfully!!!")
 
 
 def total_scratchcards_main():
